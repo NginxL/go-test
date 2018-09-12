10848702bb40e0e8d573532b09d38abe3cffcaa3
NginxL <56709951@qq.com>
2018-09-12 16:50:07 +0800

    add errors

--- src/main/main.go
+++ src/main/main.go
@@ -6,9 +6,11 @@ import (
 	"time"
 
 	"github.com/BurntSushi/toml"
+	"github.com/juju/errors"
 	"github.com/siddontang/go-log/log"
 )
 
+// test toml
 type tomlConfig struct {
 	Title   string
 	Owner   ownerInfo
@@ -41,7 +43,32 @@ type clients struct {
 	Hosts []string
 }
 
+// test toml end
+
+// test log
 var logLevel = flag.String("log_level", "info", "log level")
+
+// test log end
+
+// test errors
+func error_test(arg int) (int, error) {
+	if arg < 0 {
+		return -1, errors.Errorf("arg is not right:%d", arg)
+	} else {
+		return arg * arg, nil
+	}
+}
+
+func error_test2(arg int) (int, error) {
+	arg, err := error_test(arg)
+	if err != nil {
+		//return -1, errors.Trace(err)
+		return -1, errors.Annotate(err, "error in test2")
+	}
+	return arg, nil
+}
+
+// test errors end
 
 func main() {
 	flag.Parse()
@@ -67,5 +94,12 @@ func main() {
 	fmt.Printf("log level:%s\n", *logLevel)
 	log.SetLevelByName(*logLevel)
 	log.Info("info")
+	log.Infof("info with %s", "some info")
 	log.Error("error")
+	log.Errorf("error with %s", "some string")
+
+	_, err := error_test2(-1)
+	if err != nil {
+		log.Error(errors.ErrorStack(err))
+	}
 }
